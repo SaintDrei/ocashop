e03e12c124c98fcb4b9218de12845a05e59a2858
Santos <11356839@benilde.edu.ph>
2018-03-01 21:00:24 +0800

    Push out

--- dashboard.php
+++ dashboard.php
@@ -1,53 +1,12 @@
 
 <?php 
-//	$page_title = "Dashboard";
-//	include_once('../includes/header_admin.php');
-//    
+	$page_title = "Dashboard";
+	include_once('../includes/header_admin.php');
+    
 ?>
 
-<!----------------------------------- HEADER START ----------------------------------->
-<?php
-    ob_start(); # Quick fix to 'Warning: Cannot modify header information - headers already sent by...'
-    
-    # sets path of application folder
-    $protocol  = empty($_SERVER['HTTPS']) ? 'http' : 'https';
-    $port      = $_SERVER['SERVER_PORT'];
-    $disp_port = ($protocol == 'http' && $port == 80 || $protocol == 'https' && $port == 443) ? '' : ":$port";
-    $domain    = $_SERVER['SERVER_NAME'];
 
-    define('app_path', "${protocol}://${domain}${disp_port}" . '/casppr/');
 
-    require($_SERVER['DOCUMENT_ROOT'] . '/casppr/config.php');
-    require($_SERVER['DOCUMENT_ROOT'] . '/casppr/function.php');
-?>
-<!DOCTYPE html> 
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <title><?php echo $page_title ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta name="author" content="Andrei Mishael D. Santos">
-    <meta charset="utf-8">
-    <meta name="description" content="Official Website of DLS-CSB's Office of Culture and Arts">
-    <meta name='keywords' content="arts, theater, dance, choir, de la salle, saint, benilde, la salle, DLS-CSB">
-    <link rel="icon" href="../content/images/OCA/fav.png">
-     <!-- Stylesheets -->
-       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css">
-        <link rel="stylesheet" href="<?php echo app_path ?>materialize/css/custom.css">
-       
-        <!-- SCRIPTS -->
-      
-       
-            
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-</head>
-<body>
-    <div class="container">
-    
-    
-        <!----------------------------------- HEADER END ---------------------->
   <main>
       <div class="row">
       
@@ -187,31 +146,9 @@
       </ul>
     </div>
   </main>
-<!---------------------------------------- FOOTER START ----------------------------->
-        <footer class="blue page-footer">
- 
-    <div class="footer-copyright">
-      <div class="container">
-        <span>Made By <a class="white-text" style='font-weight: bold;' href="">TEAM QUADRA</a></span>
-      </div>
-    </div>
-  </footer>
-</div>
-      
-    
-     <!--Import jQuery before materialize.js-->
-        <!--import use echo app path to pertain to home directory because if not, the document will point to current document where footer is included instead of footer's directory-->
 
-        <script type="text/javascript" src='<?php echo app_path ?>includes/jquery-3.2.1.min.js'></script>
-        <script type="text/javascript" src='<?php echo app_path ?>materialize/js/materialize.min.js'></script>
-        <script type="text/javascript" src='<?php echo app_path ?>includes/script.js'></script>
-     
-
-</body>
-</html> 
-<!------------------------------------------ FOOTER END ---------------------------->
   
     <?php
     
-//	include_once('../includes/footclean.php');
+	include_once('../includes/footclean.php');
     ?>
