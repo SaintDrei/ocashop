6341df6a98917e396612f9105ea476213379ab7f
Andrei Santos <andreimishael.santos@benilde.edu.ph>
2018-04-13 05:33:41 +0800

    bare code for index.php

--- index.php
+++ index.php
@@ -2,7 +2,42 @@
 $page_title = 'turtle';
 include_once('/includes/header.php'); ?>
 
-
+<div class="col l10 m10 s10">
+          <div class="row">
+            <div class="col l8 push-l2">
+                    <div class="row">
+                        <div class="col l4">
+                        Books
+                        </div>
+                        <div class="col l4">
+                        Books
+                        </div>
+                        <div class="col l4">
+                        Books
+                        </div>
+                        <div class="col l4">
+                        Books
+                        </div>
+                </div>
+              </div>
+          </div>
+            <div class="row">
+                <div class="col l3">
+                    <div class="card medium" >
+                        <div class="card-image">
+                            <img src="images/card.jpg" style="max-width: 100%; max-height:100%;">
+                            <span class="card-title">Hamilton Card</span>
+                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                        </div>
+                        <div class="card-content">
+                            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                        </div>
+                
+                    </div>
+                </div>
+                
+          </div>
+      </div>
 
 
 
